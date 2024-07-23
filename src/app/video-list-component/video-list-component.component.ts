93e034bc936d8bcd6fa512020bfefd57bdaf6973
OfirDror-Nice <ofir.dror@nice.com>
2024-07-23 13:54:58 +0300

    new

--- src/app/video-list-component/video-list-component.component.ts
+++ src/app/video-list-component/video-list-component.component.ts
@@ -12,11 +12,35 @@ export class VideoListComponentComponent {
     time: "01-01-2024 08:49AM", duration: "1:09", incoming: false },
     { id: 2, agentName: 'Agent 2', image: '/assets/call.png', 
       description: 'Description of Call 2', segmentId: "e28531b5-7ebd-491f-88c4-6c26bad48d50",
-    time: "01-02-2024 04:49AM", duration: "2:19", incoming: true }
+    time: "01-02-2024 04:49AM", duration: "2:19", incoming: true },
+    { id: 3, agentName: 'Agent 3', image: '/assets/call.png', 
+      description: 'Description of Call 3', segmentId: "e28531b5-7ebd-491f-88c4-6c26bad48d50",
+    time: "01-01-2024 08:49AM", duration: "1:41", incoming: false },
+    { id: 4, agentName: 'Agent 4', image: '/assets/call.png', 
+      description: 'Description of Call 4', segmentId: "e28531b5-7ebd-491f-88c4-6c26bad48d50",
+    time: "01-02-2024 04:49AM", duration: "2:19", incoming: true },
+    { id: 5, agentName: 'Agent 5', image: '/assets/call.png', 
+      description: 'Description of Call 5', segmentId: "e28531b5-7ebd-491f-88c4-6c26bad48d50",
+    time: "20-01-2024 08:49AM", duration: "1:19", incoming: false },
+    { id: 6, agentName: 'Agent 6', image: '/assets/call.png', 
+      description: 'Description of Call 6', segmentId: "e28531b5-7ebd-491f-88c4-6c26bad48d50",
+    time: "01-03-2024 08:49AM", duration: "1:09", incoming: false },
+    { id: 7, agentName: 'Agent 7', image: '/assets/call.png', 
+      description: 'Description of Call 7', segmentId: "e28531b5-7ebd-491f-88c4-6c26bad48d50",
+    time: "01-07-2024 04:49AM", duration: "2:19", incoming: true },
+    { id: 8, agentName: 'Agent 8', image: '/assets/call.png', 
+      description: 'Description of Call 8', segmentId: "e28531b5-7ebd-491f-88c4-6c26bad48d50",
+    time: "01-11-2024 08:49AM", duration: "5:19", incoming: false },
+    { id: 9, agentName: 'Agent 9', image: '/assets/call.png', 
+      description: 'Description of Call 9', segmentId: "e28531b5-7ebd-491f-88c4-6c26bad48d50",
+    time: "11-02-2024 04:49AM", duration: "2:17", incoming: true },
+    { id: 10, agentName: 'Agent 10', image: '/assets/call.png', 
+      description: 'Description of Call 10', segmentId: "e28531b5-7ebd-491f-88c4-6c26bad48d50",
+    time: "04-02-2024 04:49AM", duration: "2:34", incoming: true },
   ];
 
   baseurl = "https://na1.dev.nice-incontact.com/player/#/cxone-player/segments/";
-  
+
   constructor(private videoService: VideoServiceComponent) { }
 
   ngOnInit() {
@@ -24,6 +48,10 @@ export class VideoListComponentComponent {
       (data) => this.videos = data,
       (error) => console.error('Error fetching videos:', error)
     );
+  }
+
+  navigateToExternalLink(link: any) {
+    window.open(this.baseurl+link.segmentId);
   }
 }
 
