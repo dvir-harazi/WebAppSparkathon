948d51557be2c8b6417e94c0eee63ae7ab3022a8
dvir-harazi <dvir.harazi@nice.com>
2024-07-23 12:59:53 +0300

    add cards

--- src/app/video-list-component/video-list-component.component.ts
+++ src/app/video-list-component/video-list-component.component.ts
@@ -6,7 +6,18 @@ import { VideoServiceComponent } from '../video-service/video-service.component'
   styleUrl: './video-list-component.component.css'
 })
 export class VideoListComponentComponent {
-  videos: any[] = [];
+  videos: any[] = [
+    { id: 1, title: 'Video 1', description: 'Description of Video 1' },
+    { id: 2, title: 'Video 2', description: 'Description of Video 2' },
+    { id: 3, title: 'Video 3', description: 'Description of Video 3' },
+    { id: 4, title: 'Video 4', description: 'Description of Video 4' },
+    { id: 5, title: 'Video 5', description: 'Description of Video 5' },
+    { id: 6, title: 'Video 6', description: 'Description of Video 6' },
+    { id: 7, title: 'Video 7', description: 'Description of Video 7' },
+    { id: 8, title: 'Video 8', description: 'Description of Video 8' },
+    { id: 9, title: 'Video 9', description: 'Description of Video 9' },
+    { id: 10, title: 'Video 10', description: 'Description of Video 10' }
+  ];
 
   constructor(private videoService: VideoServiceComponent) { }
 
