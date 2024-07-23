732d2b440f406d4b995148a72435f9effb73e410
OfirDror-Nice <ofir.dror@nice.com>
2024-07-23 13:08:38 +0300

    add

--- src/app/video-list-component/video-list-component.component.ts
+++ src/app/video-list-component/video-list-component.component.ts
@@ -7,16 +7,16 @@ import { VideoServiceComponent } from '../video-service/video-service.component'
 })
 export class VideoListComponentComponent {
   videos: any[] = [
-    { id: 1, title: 'Video 1', description: 'Description of Video 1' },
-    { id: 2, title: 'Video 2', description: 'Description of Video 2' },
-    { id: 3, title: 'Video 3', description: 'Description of Video 3' },
-    { id: 4, title: 'Video 4', description: 'Description of Video 4' },
-    { id: 5, title: 'Video 5', description: 'Description of Video 5' },
-    { id: 6, title: 'Video 6', description: 'Description of Video 6' },
-    { id: 7, title: 'Video 7', description: 'Description of Video 7' },
-    { id: 8, title: 'Video 8', description: 'Description of Video 8' },
-    { id: 9, title: 'Video 9', description: 'Description of Video 9' },
-    { id: 10, title: 'Video 10', description: 'Description of Video 10' }
+    { id: 1, agentName: 'Agent 1', image: '/assets/call.png', description: 'Description of Video 1' },
+    { id: 2, agentName: 'Agent 2', image: '/assets/call.png', description: 'Description of Video 2' },
+    { id: 3, agentName: 'Agent 3', image: '/assets/call.png', d: 'Description of Video 3' },
+    { id: 4, agentName: 'Agent 4', image: '/assets/call.png', d: 'Description of Video 4' },
+    { id: 5, agentName: 'Agent 5', image: '/assets/call.png', d: 'Description of Video 5' },
+    { id: 6, agentName: 'Agent 6', image: '/assets/call.png', dscription: 'Description of Video 6' },
+    { id: 7, agentName: 'Agent 7', image: '/assets/call.png', d: 'Description of Video 7' },
+    { id: 8, agentName: 'Agent 8', image: '/assets/call.png', d: 'Description of Video 8' },
+    { id: 9, agentName: 'Agent 9', image: '/assets/call.png', d: 'Description of Video 9' },
+    { id: 10, agentName: 'Agent 10', image: '/assets/call.png', d: 'Description of Video 10' }
   ];
 
   constructor(private videoService: VideoServiceComponent) { }
